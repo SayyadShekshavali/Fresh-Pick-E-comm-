a169d9b1f2b261c12f5926f8a1f8cb8b35b5c56e
Sayyad sheksha <syedshaikshavali@gmail.com>
2025-08-17 15:13:50 +0530

    Remove items2

--- client/src/store/CartStore.js
+++ client/src/store/CartStore.js
@@ -67,7 +67,7 @@ export const CartStore = create(() => ({
     console.log(id);
     try {
       const res = await axios.post(
-        `${import.meta.env.VITE_API_URL}/itemdelete`,
+        `${import.meta.env.VITE_API_URL}cart/itemdelete`,
         { id }
       );
       if (res.data.success) {
